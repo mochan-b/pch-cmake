43fe723e947d63c06e078112bb539ec90b83c6a5
Ubuntu <ubuntu@jekyll.lxd>
2019-12-10 22:41:25 +0000

    Precompiled headers test project

---
+++ catch-test.cpp
@@ -0,0 +1,4 @@
+
+TEST_CASE( "Two and Two is Four", "[2+2=4]" ) {
+    REQUIRE( 2+2 == 3 );
+}
---
+++ hello.h
@@ -0,0 +1,6 @@
+#include <iostream>
+
+template<typename T>
+void say(T t) {
+	std::cout << 't' << t << std::endl;
+}
